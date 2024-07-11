cc9dcac4895bfdb2f96adf8377b5942d081f46a8
jr-programmer <expertmazahrul@gmail.com>
2024-07-11 20:58:30 +0600

    4t

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,4 +1,6 @@
 import { Anek_Bangla } from "next/font/google";
+import { FaRegCircleQuestion } from "react-icons/fa6";
+import { fethcData } from "../action";
 
 const tiroBangla = Anek_Bangla({
   style: "normal",
@@ -6,11 +8,12 @@ const tiroBangla = Anek_Bangla({
   subsets: ["bengali"],
 });
 
-import Testform from "./Testform";
+
 
 
 export default async function page() {
-
+  const UserData = await fethcData()
+const {data}=await UserData;
 
 
   return (
@@ -31,7 +34,27 @@ export default async function page() {
           </p>
         </div>
 
-       <Testform />
+
+        {data && data.map((item,i) => (
+       <div key={i} className="text-gray-400 rounded-md  m-4 border border-gray-700 w-[94%] mx-auto md:w-[80%]">
+  <p className="text-center font-semibold text-emerald-600 rounded-md bg-slate-800 p-2"> {i+1}</p>
+         <div
+          
+          className=" my-6  px-4  shadow-md shadow-gray-900"
+        >
+          <h2 className="text-lg flex gap-2 items-center py-3 font-semibold">
+            <FaRegCircleQuestion className="text-emerald-600	 text-2xl" />
+            নিচের কোডে কি ভুল আছে?
+          </h2>
+          <p  className="w-[80%] mx-auto">
+            ধরে নেই আমাদের দুটি .js ফাইল আছে। একটি হলো sum.js এবং আরেকটি হলো
+            index.js এবং দুটি ফাইল একই ফোল্ডারে আছে। index.js ফাইলটি তে sum()
+            ফাংশনটি কল করে সঠিকভাবে আউটপুট দেখানোর ক্ষেত্রে নিচের কোডে কি ভুল
+            আছে?
+          </p>
+        </div>
+       </div>
+      ))}
 
 
       </div>
